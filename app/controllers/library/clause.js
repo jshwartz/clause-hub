088e3ebca9b1dfd95f5a56b9e6a72b459246ea49
Jacob Shwartz <jshwartz@gmail.com>
2016-10-13 15:28:19 -0700

    fixed router loading issue

--- app/controllers/library/clause.js
+++ app/controllers/library/clause.js
@@ -2,13 +2,28 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   titlesChanged: false,
+  modelChanged: Ember.observer('model.title', function() {
+    // deal with the change
+    this.toggleProperty('titlesChanged');
+    console.log(`modelchanged`);
+  }),
   blockArray: Ember.computed('titlesChanged', function() {
-    const blocks = this.get('model.blocks');
     let result = [];
-    blocks.forEach( (block) => {
-      const type = block.get('title');
-      const orderNumber = block.get('orderNumber');
-      result.addObject({order: orderNumber, text: type});
+    this.get('model.blocks').then(function(blocks){
+      blocks.forEach( (block) => {
+        const type = block.get('type');
+        const orderNumber = block.get('orderNumber');
+        const helpText = block.get('helpText');
+        const title = block.get('title');
+        let text = '';
+        if (type === 'dropdown') {
+          const dropdowns = block.get('block.blockDropdowns');
+          dropdowns.forEach( (dropdown) => {
+            console.log('dropdown');
+          });
+        }
+        result.addObject({order: orderNumber, type: type, helpText: helpText, title: title});
+      });
     });
     return result;
   }),
@@ -18,7 +33,7 @@ export default Ember.Controller.extend({
     const sortedBlockArray = this.get('sortedBlockArray');
     let result = "";
     for (let object of sortedBlockArray) {
-      result = result.concat(object.text);
+      result = result.concat(object.type);
     }
     return result;
   }),
--- app/routes/library/clause.js
+++ app/routes/library/clause.js
@@ -4,13 +4,6 @@ import RSVP from 'rsvp';
 export default Ember.Route.extend({
 
   model(params) {
-     return RSVP.hash({
-       clause: this.get('store').findRecord('clause', params.clause_id),
-       blocks: this.get('store').query('block', { clause: params.clause_id })
-     });
-   }
-
-
-
-
+     return this.get('store').findRecord('clause', params.clause_id)
+   },
 });
